bc73210f62a47fec0ac53c20376daef4590cbcd7
Tatr1 <ttttt_68@mail.ru>
2021-11-28 22:41:38 +0300

    edited all

---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,69 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    static private Faker faker = new Faker(new Locale("ru"));
+
+     public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+      City randomCity = new City();
+        String city = randomCity.randomChoiceCity();
+        return city;
+    }
+
+    public static String generateName() {
+         String name = faker.name().fullName();
+        return name;
+    }
+
+    public static String generatePhone() {
+       String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser() {
+          UserInfo user = new UserInfo();
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city = generateCity();
+        String name = generateName();
+        String phone = generatePhone();
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+
+    }
+
+}
+
